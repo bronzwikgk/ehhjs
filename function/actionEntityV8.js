fde56be5482853fe20c39a0fa1b8f4238dae9b3c
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-09 17:17:41 +0530

    u

--- function/actionEntityV8.js
+++ function/actionEntityV8.js
@@ -107,5 +107,35 @@ class Entity {
         return response;
     } 
 }
+class EntityModel {
+
+    constructor() { this.entityCollection = JSON.parse(localStorage.getItem('entityCollection')) || new WeakSet(); }
+
+    addItem(entity, value) {
+        if (this.hasItem(entity)) {
+            throw new Error(
+                `The entity can only contain one instance of item ${entity}`
+            );
+        }
+        // console.log(JSON.stringify(this));
+        this.entityCollection.add(entity);
+        //  console.log()
+        window.localStorage.setItem(entity, value);
+    }
+
+    removeItem(entity) {
+        return this.entityCollection.delete(entity);
+    }
+    hasItem(entity) {
+        return this.entityCollection.has(entity);
+    }
+    getItem(entity) {
+        return this.entityCollection.get(entity);
+    }
+    clearItem(entity) {
+        return
+    }
+}
+
 
 //console.log("I am loaded > entity",)
--- function/actionHelpers.js
+++ function/actionHelpers.js
@@ -10,6 +10,31 @@
 
 //https://github.com/google/data-layer-helper
 //https://sites.google.com/site/scriptsexamples/custom-methods/2d-arrays-library
+//https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
+function deepMerge(target, source) {
+    if (typeof target !== 'object' || typeof source !== 'object') return false; // target or source or both ain't objects, merging doesn't make sense
+    for (var prop in source) {
+        if (!source.hasOwnProperty(prop)) continue; // take into consideration only object's own properties.
+        if (prop in target) { // handling merging of two properties with equal names
+            if (typeof target[prop] !== 'object') {
+                target[prop] = source[prop];
+            } else {
+                if (typeof source[prop] !== 'object') {
+                    target[prop] = source[prop];
+                } else {
+                    if (target[prop].concat && source[prop].concat) { // two arrays get concatenated
+                        target[prop] = target[prop].concat(source[prop]);
+                    } else { // two objects get merged recursively
+                        target[prop] = deepMerge(target[prop], source[prop]);
+                    }
+                }
+            }
+        } else { // new properties get added to target
+            target[prop] = source[prop];
+        }
+    }
+    return target;
+}
 
 
 //return true if all items are the same in two unordered Array need to add a return of mismatch values as option.
