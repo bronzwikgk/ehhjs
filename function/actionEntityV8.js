e0ebb177494468bb1ded4c005834be0e04b9c8d0
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-14 12:50:47 +0530

    u

--- function/actionEntityV8.js
+++ function/actionEntityV8.js
@@ -108,56 +108,33 @@ class Entity {
     }
     static get(key, parent) {
         // console.log("for Initaition", key, objectModel, objectModel[key])
+
         if (parent[key]) {
             // console.log("for Initaition", key, objectModel, objectModel[key])
             var response = parent[key];
+            
             // console.log("Initaites found",response)
             return response;
+        } else {
+            return console.log("objectNotfound");
         }
 
     }
     //https://gomakethings.com/how-to-get-the-value-of-an-object-from-a-specific-path-with-vanilla-js/#:~:text=return%20our%20match.-,var%20get%20%3D%20function%20(obj%2C%20path%2C%20def)%20%7B,(or%20null)%20if%20(!
-    static get4rmPath(key,path){
-        var get = function (obj, path, def) {
+    static get4rmPath(obj, path, def){
+        
 
             /**
              * If the path is a string, convert it to an array
              * @param  {String|Array} path The path
              * @return {Array}             The path array
              */
-            var stringToPath = function (path) {
-        
-                // If the path isn't a string, return it
-                if (typeof path !== 'string') return path;
-        
-                // Create new array
-                var output = [];
-        
-                // Split to an array with dot notation
-                path.split('.').forEach(function (item, index) {
-        
-                    // Split to an array with bracket notation
-                    item.split(/\[([^}]+)\]/g).forEach(function (key) {
-        
-                        // Push to the new array
-                        if (key.length > 0) {
-                            output.push(key);
-                        }
-        
-                    });
-        
-                });
-        
-                return output;
-        
-            };
-        
+           
             // Get the path as an array
-            path = stringToPath(path);
-        
+        path = Entity.stringToPath(path);
+        console.log(path)
             // Cache the current object
             var current = obj;
-        
             // For each item in the path, dig into the object
             for (var i = 0; i < path.length; i++) {
         
@@ -168,11 +145,35 @@ class Entity {
                 current = current[path[i]];
         
             }
-        
             return current;
         
-        };
+        
     }
+     static stringToPath (path) {
+
+    // If the path isn't a string, return it
+    if (typeof path !== 'string') return path;
+    // Create new array
+    var output = [];
+
+    // Split to an array with dot notation
+    path.split('.').forEach(function (item, index) {
+
+        // Split to an array with bracket notation
+        item.split(/\[([^}]+)\]/g).forEach(function (key) {
+
+            // Push to the new array
+            if (key.length > 0) {
+                output.push(key);
+            }
+
+        });
+
+    });
+
+    return output;
+
+};
     /*!
  * Create a new object composed of properties that meet specific criteria
  * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
