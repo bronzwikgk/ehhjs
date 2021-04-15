88dd1d05c07ef49529c84c90c49ed1896e48b295
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-15 18:52:25 +0530

    u

--- function/editorV1.js
+++ function/editorV1.js
@@ -1,7 +1,10 @@
 class Editor{
     constructor(element) {
-        this._editor = [element],
+        this._editor = [element]
         //this._editor['blocks'] = "", // query selector command to be input.
+    }
+    getCaret(){
+
     }
 
 }
