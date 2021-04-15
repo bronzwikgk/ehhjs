ecddc7da2c54ba723318db5937a300595cdb1e48
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-15 19:25:50 +0530

    u

--- function/editorV1.js
+++ function/editorV1.js
@@ -1,17 +1,21 @@
 class Editor{
     constructor(element) {
         this._editor = [element]
+        console.log(element)
         //this._editor['blocks'] = "", // query selector command to be input.
         document.addEventListener("keydown", function(e) {
             console.log(e.which);
+            editor.getCaret(e);
+          })
+          window.addEventListener("change", function(e) {
+            console.log(e.which,e.type);
           })
     }
-    getCaret(){
-
+    getCaret(e){
+        console.log(console.type,e.target)
     }
-
 }
 
-var editor = new Editor('#actionEditor', { theme: 'snow' });
+var editor = new Editor(window['actionEditor'], { theme: 'snow' });
 
 
