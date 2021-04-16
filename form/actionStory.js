f48831732b877f8c1a98dd1f4507b4f73fb8efcd
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-16 13:56:55 +0530

    u

--- form/actionStory.js
+++ form/actionStory.js
@@ -30,7 +30,10 @@ var sampleIntroStory = `Honour the past
 Be honest about the present
 Be hopeful about the future
 
-The best teams in the world all have one thing in common – they live and breathe their purpose and brand promise, inside and out. This is their ‘brand culture’. Much more than just a great company culture, a true brand culture connects your employee experience with your customer experience, and ensures that it’s all pointing towards your purpose as a north star – to accelerate growth, or drive change.
+The best teams in the world all have one thing in common – they live and breathe their purpose and brand promise, inside and out.
+This is their ‘brand culture’.
+Much more than just a great company culture, a true brand culture connects your employee experience with your customer experience,
+ and ensures that it’s all pointing towards your purpose as a north star – to accelerate growth, or drive change.
 
 Shunya.ek
 Stop.go.think
